44e18eca8a579caa57fc4cd0d0c30571ddcf1659
Hany Medhat <hany.medhat24@gmail.com>
2024-09-21 16:00:03 +0300

    Update SnackBar styling and add user persistence
    - Modified `showSnackBar` function in `utils.dart` to set background color based on error status and updated text color to white.
    - Enhanced `AuthService` in `auth_service.dart` to save user data and token using `SharedPreferences` and `UserProvider`.
    - Added `UserProvider` class in `user_provider.dart` to manage user state and notify listeners on user data change.
    - Updated `GeneratedPluginRegistrant.swift` to register `SharedPreferencesPlugin`.
    - Added and updated dependencies in `pubspec.yaml` and `pubspec.lock` to include `provider` and `shared_preferences` packages.

--- lib/constant/utils.dart
+++ lib/constant/utils.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String text, {bool error = false}) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      backgroundColor: error ? Colors.red : Colors.green,
       content: Text(
-    text,
-    textAlign: TextAlign.center,
-    style: TextStyle(
-      color: error ? Colors.red : Colors.green,
-      fontSize: 16.0,
-    ),
-  )));
+        text,
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+        ),
+      )));
 }
--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -4,8 +4,11 @@ import 'package:amazon_clone/constant/error_handling.dart';
 import 'package:amazon_clone/constant/global_variable.dart';
 import 'package:amazon_clone/constant/utils.dart';
 import 'package:amazon_clone/models/user.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   // sign up user
@@ -37,8 +40,11 @@ class AuthService {
       httpErrorHandel(
         response: res,
         context: context,
-        onSuccess: () {
+        onSuccess: () async {
           showSnackBar(context, 'Account created! Login with same credentials');
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+          await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
         },
       );
     } catch (e) {
@@ -63,9 +69,11 @@ class AuthService {
           'Content-Type': 'application/json; charset=UTF-8',
         },
       );
+
+      debugPrint('res: $res');
       httpErrorHandel(response: res, context: context, onSuccess: () async {
         showSnackBar(context, 'Login Successful');
-        
+
       });
     } catch (e) {
       showSnackBar(context, e.toString(), error: true);
