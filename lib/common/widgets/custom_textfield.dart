fa4c029cc11b2b3cc1fa45c43128a000730e3967
Hany Medhat <hany.medhat24@gmail.com>
2024-09-27 01:44:08 +0300

    Add multi-line support and category selection to Add Product Screen
    - Modified `CustomTextfield` widget to accept `maxLines` parameter, defaulting to 1.
    - Updated `CustomTextfield` decoration to include a grey hint text style.
    - Introduced new fields in `AddProductScreen` for product details: name, description, price, and quantity.
    - Added a `DottedBorder` widget for product image selection.
    - Implemented a dropdown menu for selecting product categories.
    - Added a `CustomButton` for submitting the product details.
    - Managed state and controller disposal for text fields.

--- lib/common/widgets/custom_textfield.dart
+++ lib/common/widgets/custom_textfield.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 
 class CustomTextfield extends StatelessWidget {
   final TextEditingController controller;
+  final int maxLines;
   final String hintText;
   const CustomTextfield(
-      {super.key, required this.controller, required this.hintText});
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      this.maxLines = 1});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      maxLines: maxLines,
       controller: controller,
       decoration: InputDecoration(
           hintText: hintText,
+          hintStyle: const TextStyle(color: Colors.grey),
           border: const OutlineInputBorder(
             borderSide: BorderSide(
               color: Colors.black,
--- lib/features/admin/screens/add_product_screens.dart
+++ lib/features/admin/screens/add_product_screens.dart
@@ -1,4 +1,7 @@
+import 'package:amazon_clone/common/widgets/custom_button.dart';
+import 'package:amazon_clone/common/widgets/custom_textfield.dart';
 import 'package:amazon_clone/constant/global_variable.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class AddProductScreen extends StatefulWidget {
@@ -10,6 +13,27 @@ class AddProductScreen extends StatefulWidget {
 }
 
 class _AddProductScreenState extends State<AddProductScreen> {
+  final TextEditingController productNameController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController priceController = TextEditingController();
+  final TextEditingController quantityController = TextEditingController();
+  String category = 'Mobiles';
+  @override
+  void dispose() {
+    super.dispose();
+    productNameController.dispose();
+    descriptionController.dispose();
+    priceController.dispose();
+    quantityController.dispose();
+  }
+
+  List<String> productCategories = [
+    'Mobiles',
+    'Essentials',
+    'Appliances',
+    'Books',
+    'Fashion',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +51,99 @@ class _AddProductScreenState extends State<AddProductScreen> {
               style: TextStyle(
                 color: Colors.black,
               ),
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                DottedBorder(
+                  borderType: BorderType.RRect,
+                  radius: const Radius.circular(10),
+                  dashPattern: const [10, 4],
+                  strokeCap: StrokeCap.round,
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.folder_open,
+                          size: 40,
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          'Select Product Images',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                CustomTextfield(
+                  controller: productNameController,
+                  hintText: 'Product Name',
+                ),
+                const SizedBox(height: 10),
+                CustomTextfield(
+                  controller: descriptionController,
+                  hintText: 'Description',
+                  maxLines: 7,
+                ),
+                const SizedBox(height: 10),
+                CustomTextfield(
+                  controller: priceController,
+                  hintText: 'Price',
+                ),
+                const SizedBox(height: 10),
+                CustomTextfield(
+                  controller: quantityController,
+                  hintText: 'Quantity',
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: double.infinity,
+                  child: DropdownButtonHideUnderline(
+
+                    child: DropdownButton(
+                      dropdownColor: Colors.white,
+                      items: productCategories.map((String value) {
+                        return DropdownMenuItem(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      value: category,
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      onChanged: (String? value) {
+                        setState(
+                          () {
+                            category = value!;
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                CustomButton(
+                  text: 'Sell',
+                  onPressed: () {},
+                ),
+              ],
             ),
           ),
         ),
