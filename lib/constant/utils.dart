58b8c580a3e7fd9b7378ab5ce75d129cf27cbd3e
Hany Medhat <hany.medhat24@gmail.com>
2024-09-27 19:04:32 +0300

    Add image picker functionality for product images
    This commit introduces the ability to select and manage product images in the admin section of the application. The following changes were made:
    
    - Added `pickImages` function in `utils.dart` to handle the selection of multiple images using the `file_picker` package.
    - Integrated the `pickImages` function into the `AddProductScreen` to allow admins to select images for new products.
    - Updated the UI in `AddProductScreen` to include a gesture detector for selecting images and displaying the selected images.
    - Added necessary dependencies (`file_picker`, `cross_file`, `flutter_plugin_android_lifecycle`, `win32`) to `pubspec.yaml` and updated `pubspec.lock` accordingly.
    
    This feature enhances the admin experience by providing a straightforward way to upload product images.

--- lib/constant/utils.dart
+++ lib/constant/utils.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
 void showSnackBar(BuildContext context, String text, {bool error = false}) {
   ScaffoldMessenger.of(context).showSnackBar(
@@ -15,3 +17,40 @@ void showSnackBar(BuildContext context, String text, {bool error = false}) {
     ),
   );
 }
+
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+  try {
+    var files = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: true,
+    );
+
+    if (files != null && files.files.isNotEmpty) {
+      for (int i = 0; i < files.files.length; i++) {
+        images.add(File(files.files[i].path!));
+      }
+    }
+  } catch (e) {
+    debugPrint(e.toString());
+  }
+  return images;
+}
+  // return await showModalBottomSheet(
+  //   context: Get.context!,
+  //   builder: (context) => Column(
+  //     children: [
+  //       ListTile(
+  //         title: const Text('Take a photo'),
+  //         leading: const Icon(Icons.camera),
+  //         onTap: () {},
+  //       ),
+  //       ListTile(
+  //         title: const Text('Choose from gallery'),
+  //         leading: const Icon(Icons.image),
+  //         onTap: () {},
+  //       ),
+  //     ],
+  //   ),
+  // );
+
--- lib/features/admin/screens/add_product_screens.dart
+++ lib/features/admin/screens/add_product_screens.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:amazon_clone/common/widgets/custom_button.dart';
 import 'package:amazon_clone/common/widgets/custom_textfield.dart';
 import 'package:amazon_clone/constant/global_variable.dart';
+import 'package:amazon_clone/constant/utils.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
@@ -18,6 +21,7 @@ class _AddProductScreenState extends State<AddProductScreen> {
   final TextEditingController priceController = TextEditingController();
   final TextEditingController quantityController = TextEditingController();
   String category = 'Mobiles';
+  List<File> productImages = [];
   @override
   void dispose() {
     super.dispose();
@@ -34,6 +38,16 @@ class _AddProductScreenState extends State<AddProductScreen> {
     'Books',
     'Fashion',
   ];
+  void selectImages() async {
+    var res = await pickImages();
+    debugPrint('res: $res');
+    debugPrint('');
+
+    setState(() {
+      productImages = res;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,33 +76,36 @@ class _AddProductScreenState extends State<AddProductScreen> {
             child: Column(
               children: [
                 const SizedBox(height: 20),
-                DottedBorder(
-                  borderType: BorderType.RRect,
-                  radius: const Radius.circular(10),
-                  dashPattern: const [10, 4],
-                  strokeCap: StrokeCap.round,
-                  child: Container(
-                    width: double.infinity,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.folder_open,
-                          size: 40,
-                        ),
-                        SizedBox(height: 15),
-                        Text(
-                          'Select Product Images',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.grey,
+                GestureDetector(
+                  onTap: selectImages,
+                  child: DottedBorder(
+                    borderType: BorderType.RRect,
+                    radius: const Radius.circular(10),
+                    dashPattern: const [10, 4],
+                    strokeCap: StrokeCap.round,
+                    child: Container(
+                      width: double.infinity,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.folder_open,
+                            size: 40,
                           ),
-                        ),
-                      ],
+                          SizedBox(height: 15),
+                          Text(
+                            'Select Product Images',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -117,7 +134,6 @@ class _AddProductScreenState extends State<AddProductScreen> {
                 SizedBox(
                   width: double.infinity,
                   child: DropdownButtonHideUnderline(
-
                     child: DropdownButton(
                       dropdownColor: Colors.white,
                       items: productCategories.map((String value) {
