8aaf532be7f1fa679bbf8b128cc831b12f4ab59b
Hany Medhat <hany.medhat24@gmail.com>
2024-10-01 19:10:15 +0300

    Refactor CategoryDealsScreen to Fetch and Display Category Products
    - Implemented fetching of products by category using `HomeServices`.
    - Added `fetchCategoryProducts` method to initialize product data on screen load.
    - Updated the UI to display a loading indicator while fetching products.
    - Enhanced the UI to display product images, names, and prices in a grid layout.
    - Created `HomeServices` class to handle HTTP requests for fetching category-specific products.
    - Integrated error handling and user authentication for API requests.

--- lib/features/home/screens/category_deals_screen.dart
+++ lib/features/home/screens/category_deals_screen.dart
@@ -1,4 +1,6 @@
 import 'package:amazon_clone/constant/global_variable.dart';
+import 'package:amazon_clone/features/home/services/home_services.dart';
+import 'package:amazon_clone/models/product.dart';
 import 'package:flutter/material.dart';
 
 class CategoryDealsScreen extends StatefulWidget {
@@ -11,6 +13,22 @@ class CategoryDealsScreen extends StatefulWidget {
 }
 
 class _CategoryDealsScreenState extends State<CategoryDealsScreen> {
+  List<Product>? products;
+  HomeServices homeServices = HomeServices();
+  @override
+  void initState() {
+    super.initState();
+    fetchCategoryProducts(category: widget.category);
+  }
+
+  void fetchCategoryProducts({required String category}) async {
+    products = await homeServices.fetchCategoryProducts(
+      context: context,
+      category: category,
+    );
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,39 +49,89 @@ class _CategoryDealsScreenState extends State<CategoryDealsScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 15,
-              vertical: 10,
+      body: products == null
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 10,
+                  ),
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Keep shopping for ${widget.category}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 170,
+                  child: GridView.builder(
+                    itemCount: products!.length,
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.only(left: 15),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      childAspectRatio: 1.4,
+                      mainAxisSpacing: 10,
+                    ),
+                    itemBuilder: (context, index) {
+                      final product = products![index];
+                      return Column(
+                        children: [
+                          SizedBox(
+                            height: 130,
+                            child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.black12,
+                                  width: 0.5,
+                                ),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(
+                                  product.images[0],
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.topLeft,
+                            padding: const EdgeInsets.only(
+                                left: 0, top: 5, right: 15),
+                            child: Text(
+                              product.name,
+                              style: const TextStyle(fontSize: 17),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          // Container(
+                          //   alignment: Alignment.topLeft,
+                          //   padding: const EdgeInsets.only(
+                          //       left: 0, top: 0, right: 15),
+                          //   child: Text(
+                          //     product.price.toString(),
+                          //     style: const TextStyle(
+                          //         fontSize: 8, color: Colors.grey),
+                          //     maxLines: 1,
+                          //     overflow: TextOverflow.ellipsis,
+                          //   ),
+                          // ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-            alignment: Alignment.topLeft,
-            child: Text(
-              'Keep shopping for ${widget.category}',
-              style: const TextStyle(
-                fontSize: 20,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 170,
-            child: GridView.builder(
-              itemCount: 10,
-              scrollDirection: Axis.horizontal,
-              padding: const EdgeInsets.only(left: 15),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-                childAspectRatio: 1.4,
-                mainAxisSpacing: 10,
-              ),
-              itemBuilder: (context, index) {
-                return const Text('Hello');
-              },
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/features/home/services/home_services.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:amazon_clone/constant/error_handling.dart';
+import 'package:amazon_clone/constant/global_variable.dart';
+import 'package:amazon_clone/constant/utils.dart';
+import 'package:amazon_clone/models/product.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+
+class HomeServices {
+  Future<List<Product>> fetchCategoryProducts(
+      {required BuildContext context, required String category}) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false).user;
+    List<Product> productList = [];
+    try {
+      http.Response res = await http.get(
+          Uri.parse('$uri/product/get-products?category=$category'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+            // ignore: use_build_context_synchronously
+            'x-auth-token': userProvider.token
+          });
+      httpErrorHandel(
+          response: res,
+          // ignore: use_build_context_synchronously
+          context: context,
+          onSuccess: () {
+            for (int i = 0; i < jsonDecode(res.body).length; i++) {
+              productList.add(
+                Product.fromJson(
+                  jsonEncode(jsonDecode(res.body)[i]),
+                ),
+              );
+            }
+          });
+    } catch (e) {
+      debugPrint(e.toString());
+      // ignore: use_build_context_synchronously
+      showSnackBar(context, e.toString(), error: true);
+    }
+    return productList;
+  }
+}
